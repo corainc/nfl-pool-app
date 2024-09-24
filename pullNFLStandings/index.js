0456384176bcbf0ef08a82656c27df24301fac3e
corainc <mcora@corainc.com>
2024-09-24 16:17:51 -0500

    Update index.js with 1o preview fixes

--- pullNFLStandings/index.js
+++ pullNFLStandings/index.js
@@ -1,3 +1,5 @@
+// pullNFLStandings/index.js
+
 const axios = require('axios');
 const { DefaultAzureCredential } = require('@azure/identity');
 const sql = require('mssql');
@@ -20,7 +22,7 @@ module.exports = async function (context, myTimer) {
             return;
         }
 
-        // Use Managed Identity (MSI) to connect to SQL Database
+        // Use Managed Identity to connect to SQL Database
         const credential = new DefaultAzureCredential();
         const accessToken = await credential.getToken("https://database.windows.net/");
         const sqlConfig = {
@@ -31,81 +33,120 @@ module.exports = async function (context, myTimer) {
                 enableArithAbort: true
             },
             authentication: {
-                type: 'azure-active-directory-msi-app-service',
+                type: 'azure-active-directory-access-token',
                 options: {
                     token: accessToken.token
                 }
             }
         };
 
-        context.log("Attempting to connect to the database...");
-
-        await sql.connect(sqlConfig);
-        context.log("Connected to the database successfully.");
+        context.log("Attempting to connect to the database with retries...");
+        await connectWithRetry(sqlConfig, context);
 
         for (const teamData of teams) {
             const { team, stats, overallRank, conferenceRank, divisionRank, playoffRank } = teamData;
 
             // Calculate the point differential
-            const pointDifferential = stats.standings.pointsFor - stats.standings.pointsAgainst;
+            const pointDifferential = (stats.standings.pointsFor || 0) - (stats.standings.pointsAgainst || 0);
 
-            // Construct the query for inserting or updating the Standings table
+            // Prepare SQL parameters
+            const request = new sql.Request();
+            request.input('TeamID', sql.Int, team.id);
+            request.input('Wins', sql.Int, stats.standings.wins || 0);
+            request.input('Losses', sql.Int, stats.standings.losses || 0);
+            request.input('OTWins', sql.Int, stats.standings.otWins || 0);
+            request.input('OTLosses', sql.Int, stats.standings.otLosses || 0);
+            request.input('WinPct', sql.Decimal(5, 3), stats.standings.winPct || 0);
+            request.input('PointsFor', sql.Int, stats.standings.pointsFor || 0);
+            request.input('PointsAgainst', sql.Int, stats.standings.pointsAgainst || 0);
+            request.input('PointDifferential', sql.Int, pointDifferential);
+            request.input('Abbreviation', sql.NVarChar(10), team.abbreviation || null);
+            request.input('Conference', sql.NVarChar(50), conferenceRank?.conferenceName || null);
+            request.input('Division', sql.NVarChar(50), divisionRank?.divisionName || null);
+            request.input('ConferenceRank', sql.Int, conferenceRank?.rank || null);
+            request.input('DivisionRank', sql.Int, divisionRank?.rank || null);
+            request.input('Ties', sql.Int, stats.standings.ties || 0);
+            request.input('OfficialLogoURL', sql.NVarChar(255), team.officialLogoImageSrc || null);
+            request.input('SocialMedia', sql.NVarChar(100), team.socialMediaAccounts[0]?.value || null);
+            request.input('OverallRank', sql.Int, overallRank?.rank || null);
+            request.input('GamesBack', sql.Float, conferenceRank?.gamesBack || 0);
+            request.input('PlayoffRank', sql.Int, playoffRank?.rank || null);
+            request.input('HomeWins', sql.Int, stats.standings.homeWins || 0);
+            request.input('AwayWins', sql.Int, stats.standings.awayWins || 0);
+            request.input('Streak', sql.NVarChar(10), stats.standings.streak?.streakType || null);
+
             const query = `
                 MERGE INTO Standings AS target
-                USING (VALUES (
-                    ${team.id}, ${stats.standings.wins}, ${stats.standings.losses}, ${stats.standings.otWins}, 
-                    ${stats.standings.otLosses}, ${stats.standings.winPct}, ${stats.standings.pointsFor}, 
-                    ${stats.standings.pointsAgainst}, ${pointDifferential}, '${team.abbreviation}', 
-                    '${conferenceRank.conferenceName}', '${divisionRank.divisionName}', ${conferenceRank.rank || 'NULL'}, 
-                    ${divisionRank.rank || 'NULL'}, ${stats.standings.ties}, '${team.officialLogoImageSrc}', 
-                    '${team.socialMediaAccounts[0]?.value || 'NULL'}', ${overallRank.rank || 'NULL'}, 
-                    ${conferenceRank.gamesBack || 'NULL'}, ${playoffRank.rank || 'NULL'}
-                )) AS source (
-                    TeamID, Wins, Losses, OTWins, OTLosses, WinPct, PointsFor, PointsAgainst, PointDifferential, 
-                    Abbreviation, Conference, Division, ConferenceRank, DivisionRank, Ties, OfficialLogoURL, 
-                    SocialMedia, OverallRank, GamesBack, PlayoffRank
-                )
+                USING (SELECT
+                    @TeamID AS TeamID, @Wins AS Wins, @Losses AS Losses, @OTWins AS OTWins, @OTLosses AS OTLosses,
+                    @WinPct AS WinPct, @PointsFor AS PointsFor, @PointsAgainst AS PointsAgainst, @PointDifferential AS PointDifferential,
+                    @Abbreviation AS Abbreviation, @Conference AS Conference, @Division AS Division, @ConferenceRank AS ConferenceRank,
+                    @DivisionRank AS DivisionRank, @Ties AS Ties, @OfficialLogoURL AS OfficialLogoURL, @SocialMedia AS SocialMedia,
+                    @OverallRank AS OverallRank, @GamesBack AS GamesBack, @PlayoffRank AS PlayoffRank, @HomeWins AS HomeWins,
+                    @AwayWins AS AwayWins, @Streak AS Streak
+                ) AS source
                 ON target.TeamID = source.TeamID
                 WHEN MATCHED THEN
-                    UPDATE SET 
-                        Wins = source.Wins, 
-                        Losses = source.Losses, 
-                        OTWins = source.OTWins, 
-                        OTLosses = source.OTLosses, 
-                        WinPct = source.WinPct, 
-                        PointsFor = source.PointsFor, 
-                        PointsAgainst = source.PointsAgainst, 
-                        PointDifferential = source.PointDifferential, 
-                        Abbreviation = source.Abbreviation, 
-                        Conference = source.Conference, 
-                        Division = source.Division, 
-                        ConferenceRank = source.ConferenceRank, 
-                        DivisionRank = source.DivisionRank, 
-                        Ties = source.Ties, 
-                        OfficialLogoURL = source.OfficialLogoURL, 
-                        SocialMedia = source.SocialMedia, 
-                        OverallRank = source.OverallRank, 
-                        GamesBack = source.GamesBack, 
-                        PlayoffRank = source.PlayoffRank
+                    UPDATE SET
+                        Wins = source.Wins,
+                        Losses = source.Losses,
+                        OTWins = source.OTWins,
+                        OTLosses = source.OTLosses,
+                        WinPct = source.WinPct,
+                        PointsFor = source.PointsFor,
+                        PointsAgainst = source.PointsAgainst,
+                        PointDifferential = source.PointDifferential,
+                        Abbreviation = source.Abbreviation,
+                        Conference = source.Conference,
+                        Division = source.Division,
+                        ConferenceRank = source.ConferenceRank,
+                        DivisionRank = source.DivisionRank,
+                        Ties = source.Ties,
+                        OfficialLogoURL = source.OfficialLogoURL,
+                        SocialMedia = source.SocialMedia,
+                        OverallRank = source.OverallRank,
+                        GamesBack = source.GamesBack,
+                        PlayoffRank = source.PlayoffRank,
+                        HomeWins = source.HomeWins,
+                        AwayWins = source.AwayWins,
+                        Streak = source.Streak
                 WHEN NOT MATCHED THEN
-                    INSERT (TeamID, Wins, Losses, OTWins, OTLosses, WinPct, PointsFor, PointsAgainst, PointDifferential, 
-                            Abbreviation, Conference, Division, ConferenceRank, DivisionRank, Ties, OfficialLogoURL, 
-                            SocialMedia, OverallRank, GamesBack, PlayoffRank)
-                    VALUES (
-                        source.TeamID, source.Wins, source.Losses, source.OTWins, source.OTLosses, source.WinPct, 
-                        source.PointsFor, source.PointsAgainst, source.PointDifferential, source.Abbreviation, 
-                        source.Conference, source.Division, source.ConferenceRank, source.DivisionRank, source.Ties, 
-                        source.OfficialLogoURL, source.SocialMedia, source.OverallRank, source.GamesBack, source.PlayoffRank
-                    );
+                    INSERT (TeamID, Wins, Losses, OTWins, OTLosses, WinPct, PointsFor, PointsAgainst, PointDifferential,
+                            Abbreviation, Conference, Division, ConferenceRank, DivisionRank, Ties, OfficialLogoURL,
+                            SocialMedia, OverallRank, GamesBack, PlayoffRank, HomeWins, AwayWins, Streak)
+                    VALUES (source.TeamID, source.Wins, source.Losses, source.OTWins, source.OTLosses, source.WinPct,
+                            source.PointsFor, source.PointsAgainst, source.PointDifferential, source.Abbreviation,
+                            source.Conference, source.Division, source.ConferenceRank, source.DivisionRank, source.Ties,
+                            source.OfficialLogoURL, source.SocialMedia, source.OverallRank, source.GamesBack,
+                            source.PlayoffRank, source.HomeWins, source.AwayWins, source.Streak);
             `;
 
-            const request = new sql.Request();
             await request.query(query);
             context.log(`Standings data successfully written for TeamID: ${team.id}`);
         }
     } catch (error) {
-        context.log('Error fetching or updating standings:', error.message);
+        context.log.error('Error fetching or updating standings:', error.message);
+        throw error;
     } finally {
-        sql.close();
+        await sql.close();
     }
 };
+
+// Function to handle database connection with retries
+async function connectWithRetry(config, context, retries = 5, delay = 5000) {
+    for (let attempt = 1; attempt <= retries; attempt++) {
+        try {
+            await sql.connect(config);
+            context.log('Connected to the database successfully.');
+            return;
+        } catch (err) {
+            context.log(`Database connection attempt ${attempt} failed: ${err.message}`);
+            if (attempt < retries) {
+                context.log(`Retrying in ${delay / 1000} seconds...`);
+                await new Promise(res => setTimeout(res, delay));
+            } else {
+                throw err;
+            }
+        }
+    }
+}
